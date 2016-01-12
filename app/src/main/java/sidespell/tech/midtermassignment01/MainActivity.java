12ede734f360441a206b33cd3eb548c95b87e8e4
lendale <nasibag1>
2016-01-13 07:41:25 +0800

    Autocomplete textview not working properly

--- app/src/main/java/sidespell/tech/midtermassignment01/MainActivity.java
+++ app/src/main/java/sidespell/tech/midtermassignment01/MainActivity.java
@@ -8,24 +8,109 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Array of strings storing country names
+    String[] countries = new String[]{
+            "India",
+            "Pakistan",
+            "Sri Lanka",
+            "China",
+            "Bangladesh",
+            "Nepal",
+            "Afghanistan",
+            "North Korea",
+            "South Korea",
+            "Japan"
+    };
+
+    // Array of integers points to images stored in /res/drawable/
+    int[] flags = new int[]{
+            R.drawable.india,
+            R.drawable.pakistan,
+            R.drawable.srilanka,
+            R.drawable.china,
+            R.drawable.bangladesh,
+            R.drawable.nepal,
+            R.drawable.afghanistan,
+            R.drawable.nkorea,
+            R.drawable.skorea,
+            R.drawable.japan
+    };
+
+    // Array of strings to store currencies
+    String[] currency = new String[]{
+            "Indian Rupee",
+            "Pakistani Rupee",
+            "Sri Lankan Rupee",
+            "Renminbi",
+            "Bangladeshi Taka",
+            "Nepalese Rupee",
+            "Afghani",
+            "North Korean Won",
+            "South Korean Won",
+            "Japanese Yen"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // Each row in the list stores country name, currency and flag
+        List<HashMap<String, String>> aList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            HashMap<String, String> hm = new HashMap<>();
+            hm.put("txt", countries[i]);
+            hm.put("flag", Integer.toString(flags[i]));
+            hm.put("cur", currency[i]);
+            aList.add(hm);
+        }
+
+        // Keys used in Hashmap
+        String[] from = {"flag", "txt"};
+
+        // Ids of views in listview_layout
+        int[] to = {R.id.flag, R.id.txt};
+
+        // Instantiating an adapter to store each items
+        // R.layout.listview_layout defines the layout of each item
+        SimpleAdapter adapter = new SimpleAdapter(this, aList, R.layout.autocomplete_layout, from, to);
+
+        AutoCompleteTextView autoComplete = (AutoCompleteTextView) findViewById(R.id.autocomplete);
+
+        // Defining an itemclick event listener for the autocompletetextview
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
+
+                /** Each item in the adapter is a HashMap object.
+                 *  So this statement creates the currently clicked hashmap object
+                 * */
+                HashMap<String, String> hm = (HashMap<String, String>) arg0.getAdapter().getItem(position);
+
+                /** Getting a reference to the TextView of the layout file activity_main to set Currency */
+                TextView tvCurrency = (TextView) findViewById(R.id.tv_currency);
+
+                /** Getting currency from the HashMap and setting it to the textview */
+                tvCurrency.setText("Currency : " + hm.get("cur"));
             }
-        });
+        };
+
+        autoComplete.setOnItemClickListener(itemClickListener);
+
+        autoComplete.setAdapter(adapter);
+
     }
 
     @Override
